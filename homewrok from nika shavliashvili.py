deb1b431aa2dcb82f3bc88dac93c374889d8fc2e
nika shavliashvili <shavliashvilinika4@gmail.com>
2024-11-11 20:04:40 +0400

    nika

---
+++ homewrok from nika shavliashvili.py
@@ -0,0 +1,35 @@
+#num1 = eval(input("chawere ricxvi : "))
+#num2 = eval(input("chawere meore ricxvi : "))
+
+
+#jami = num1 + num2
+#nashti = num1 % num2
+
+#print(jami,nashti)
+
+
+# age=float(input("enter your age: "))
+# grade=int(input("enter your grade: "))
+# if age == 15:
+#      if grade == 10:
+#             print("you can enter in this class. ")
+# elif age > 15 :
+#      if grade > 10:
+#          print("you can not enter this class. ")
+# elif age < 15:
+#      if grade < 10:
+#           print("you can not enter this class. ")
+
+
+
+# age = int(input("sheiyvane sheni asaki:  "))
+
+# while age <18 or age >75:
+#     print("shen ar shegilia waxvide archevnebze")
+
+#     age = int(input("sheiyvane sheni asaki:  "))
+# print("shen shegilia waxvide archevnebze")
+
+
+    
+     
